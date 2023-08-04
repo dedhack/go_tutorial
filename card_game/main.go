7be58da2605867aa3e50b6e344cd8e8dcca91f03
Izhar Bin Jumadi <eezhar@gmail.com>
2023-08-04 15:09:37 +0800

    init card game

---
+++ card_game/main.go
@@ -0,0 +1,10 @@
+package main
+
+import "fmt"
+
+func main() {
+	// var card string = "Ace of Spades"
+	card := "Ace of Spades" // := is only used when declaring a new variable, infers the type
+	fmt.Println(card)
+
+}
